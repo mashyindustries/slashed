f10eaa42fcde083756c2b2922dbdec963e1175d0
Alex Khimchenko <alex.appchestrateam@gmail.com>
2018-05-10 16:28:23 +0300

    add ws routing, initial

--- test/test.js
+++ test/test.js
@@ -10,6 +10,16 @@ router.get('/', function(req, res){
     res.render('pages.home')
 })
 
+router.post('/', function(req, res){
+    res.send('ok')
+})
+
+router.ws('/test', function(ws, req){
+    ws.on('message', function(msg) {
+        console.log('test ws message', msg)
+    })
+})
+
 app.use(router)
 
 app.listen(8000)
