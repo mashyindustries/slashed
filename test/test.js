1a17ceea51f98f026a1c6451eed2661f43a6805e
Unknown <albertmashy@outlook.com>
2018-06-30 17:29:00 +0930

    Testing old master version

--- test/test.js
+++ test/test.js
@@ -1,13 +1,55 @@
 'use strict'
 
+/*var app = new app()
+
+
+function app(){
+    var stack = []
+
+    this.use = function use(fn){
+        stack.push(fn)
+    }
+
+    this.handle = async function handle(ctx){
+        try {
+            for(var i = 0; i < stack.length; i++){
+                var fn = stack[i]
+                await fn(ctx)
+            }
+        } catch (err) {
+            console.error(err)
+        }
+        
+        console.log(ctx)
+    }
+
+    return this
+}
+
+app.use(function(ctx){
+    ctx.test = 'test'
+})
+app.use(function(ctx){
+    ctx.test = 'tes2'
+})
+app.use(function(ctx){
+    ctx.test3 = 'test3'
+})
+
+
+app.handle({})
+
+*/
+
 var slashed = require('../index')
 var path = require('path')
 var basedir = __dirname
 var app = slashed(basedir)
 
 var router = slashed.Router()
-router.get('/', function(req, res){
-    res.render('pages.home')
+
+router.get('/', function(ctx){
+    ctx.res.render('pages.home')
 })
 
 router.post('/', function(req, res){
@@ -23,3 +65,5 @@ router.ws('/test', function(ws, req){
 app.use(router)
 
 app.listen(8000)
+
+//*/
