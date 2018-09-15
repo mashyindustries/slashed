4f79f462ae33e69f2fe283365f9672f66278a944
DominusVilicus <albertmashy@outlook.com>
2018-09-15 17:11:04 +0930

    starting fresh

--- test/test.js
+++ test/test.js
@@ -1,46 +1,6 @@
 'use strict'
 
-/*var app = new app()
-
-
-function app(){
-    var stack = []
-
-    this.use = function use(fn){
-        stack.push(fn)
-    }
-
-    this.handle = async function handle(ctx){
-        try {
-            for(var i = 0; i < stack.length; i++){
-                var fn = stack[i]
-                await fn(ctx)
-            }
-        } catch (err) {
-            console.error(err)
-        }
-        
-        console.log(ctx)
-    }
-
-    return this
-}
-
-app.use(function(ctx){
-    ctx.test = 'test'
-})
-app.use(function(ctx){
-    ctx.test = 'tes2'
-})
-app.use(function(ctx){
-    ctx.test3 = 'test3'
-})
-
-
-app.handle({})
-
-*/
-
+//*
 var slashed = require('../index')
 var path = require('path')
 var basedir = __dirname
@@ -49,9 +9,17 @@ var app = slashed(basedir)
 var router = slashed.Router()
 
 router.get('/', function(ctx){
-    ctx.res.render('pages.home')
+    console.log('got /')
+    ctx.res.send('test')
 })
 
+
+router.get('/lol', function(ctx){
+    console.log('got lol')
+    ctx.res.send('test')
+})
+
+/*
 router.post('/', function(req, res){
     res.send('ok')
 })
@@ -61,7 +29,7 @@ router.ws('/test', function(ws, req){
         console.log('test ws message', msg)
     })
 })
-
+*/
 app.use(router)
 
 app.listen(8000)
